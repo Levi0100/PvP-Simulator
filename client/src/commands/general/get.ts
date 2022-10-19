f9db97a67dc340f6f725b76d56e39c08c6246ea5
Levi <levi.pires2005@gmail.com>
2022-10-19 01:22:47 -0300

    Removed the console.log

--- client/src/commands/general/get.ts
+++ client/src/commands/general/get.ts
@@ -222,8 +222,6 @@ export default class GetCommand extends Command {
 
             var _armor = locale.armors[armor.type!][armor.name!]
 
-            console.log(armor)
-
             ctx.reply('commands.get.congrats2', {
               armor: `${_armor.name} ${_armor.type}`
             })
