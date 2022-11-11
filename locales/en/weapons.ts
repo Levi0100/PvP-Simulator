ffdb8324d7099a98cbaa0263809e2cc76b8a7884
Levi <levi.pires2005@gmail.com>
2022-11-11 19:13:48 -0300

    now the broad swords are normal swords

--- locales/en/weapons.ts
+++ locales/en/weapons.ts
@@ -1,40 +1,40 @@
 export const weapons = {
-  broad_sword: {
+  sword: {
     steel: {
       type: 'Steel',
-      name: 'Broad Sword of'
+      name: 'Sword of'
     },
     light: {
       type: 'Light',
-      name: 'Broad Sword of'
+      name: 'Sword of'
     },
     time: {
       type: 'Time',
-      name: 'Broad Sword of'
+      name: 'Sword of'
     },
     honor: {
       type: 'Honor',
-      name: 'Broad Sword of'
+      name: 'Sword of'
     },
     shadows: {
       type: 'Shadows',
-      name: 'Broad Sword of'
+      name: 'Sword of'
     },
     destiny: {
       type: 'Destiny',
-      name: 'Broad Sword of'
+      name: 'Sword of'
     },
     death: {
       type: 'Death',
-      name: 'Broad Sword of'
+      name: 'Sword of'
     },
     dark: {
       type: 'Negro',
-      name: 'Broad Sword'
+      name: 'Sword'
     },
     angels: {
       type: 'Angels',
-      name: 'Broad Sword of'
+      name: 'Sword of'
     }
   }
 }
--- locales/pt/weapons.ts
+++ locales/pt/weapons.ts
@@ -1,56 +1,56 @@
 export const weapons = {
-  broad_sword: {
+  sword: {
     steel: {
       type: 'Aço',
-      name: 'Espadão do',
+      name: 'Espada do',
       stars: 1,
       damage: 45
     },
     light: {
       type: 'Luz',
-      name: 'Espadão da',
+      name: 'Espada da',
       stars: 1,
       damage: 47
     },
     time: {
       type: 'Tempo',
-      name: 'Espadão do',
+      name: 'Espada do',
       stars: 2,
       damage: 56
     },
     honor: {
       type: 'Honra',
-      name: 'Espadão da',
+      name: 'Espada da',
       stars: 2,
       damage: 56
     },
     shadows: {
       type: 'Sombras',
-      name: 'Espadão das',
+      name: 'Espada das',
       stars: 3,
       damage: 61
     },
     destiny: {
       type: 'Destino',
-      name: 'Espadão do',
+      name: 'Espada do',
       stars: 3,
       damage: 60
     },
     death: {
       type: 'Morte',
-      name: 'Espadão da',
+      name: 'Espada da',
       stars: 4,
       damage: 75
     },
     dark: {
       type: 'Negro',
-      name: 'Espadão',
+      name: 'Espada',
       stars: 4,
       damage: 75
     },
     angels: {
       type: 'Anjos',
-      name: 'Espadão dos',
+      name: 'Espada dos',
       stars: 5,
       damage: 125
     }
