3cef406aee116f3a5f7fc27ef8dae5dafe94dd7e
Levi <levi.pires2005@gmail.com>
2022-11-16 16:22:11 -0300

    put a price on items

--- client/src/structures/util/simulator/armors.ts
+++ client/src/structures/util/simulator/armors.ts
@@ -3,19 +3,20 @@ export default [
     name: 'angels',
     type: 'helmet',
     stars: 5,
-    def: 22
+    def: 22,
   },
   {
     name: 'angels',
     type: 'chest',
     stars: 5,
-    def: 22
+    def: 22,
+    price: 3200
   },
   {
     name: 'angels',
     type: 'pants',
     stars: 5,
-    def: 17
+    def: 17,
   },
   {
     name: 'angels',
@@ -75,72 +76,84 @@ export default [
     name: 'blood',
     type: 'helmet',
     stars: 3,
-    def: 10
+    def: 10,
+    price: 3200
   },
   {
     name: 'blood',
     type: 'chest',
     stars: 3,
-    def: 10
+    def: 10,
+    price: 6900
   },
   {
     name: 'blood',
     type: 'pants',
     stars: 3,
-    def: 7
+    def: 7,
+    price: 6300
   },
   {
     name: 'blood',
     type: 'boots',
     stars: 3,
-    def: 7
+    def: 7,
+    price: 6300
   },
   {
     name: 'golden',
     type: 'helmet',
     stars: 2,
-    def: 7
+    def: 7,
+    price: 6300
   },
   {
     name: 'golden',
     type: 'chest',
     stars: 2,
-    def: 7
+    def: 7,
+    price: 6300
   },
   {
     name: 'golden',
     type: 'pants',
     stars: 2,
-    def: 5
+    def: 5,
+    price: 6000
   },
   {
     name: 'golden',
     type: 'boots',
     stars: 2,
-    def: 5
+    def: 5,
+    price: 6000
   },
   {
     name: 'bronze',
     type: 'helmet',
     stars: 1,
-    def: 5
+    def: 5,
+    price: 6000
   },
   {
     name: 'bronze',
     type: 'chest',
     stars: 1,
-    def: 5
+    def: 5,
+    price: 6000
   },
   {
     name: 'bronze',
     type: 'pants',
     stars: 1,
-    def: 2
+    def: 2,
+    price: 3400
   },
   {
     name: 'bronze',
     type: 'boots',
     stars: 1,
-    def: 2
+    def: 2,
+    price: 3400
   }
 ]
--- client/src/structures/util/simulator/weapons.ts
+++ client/src/structures/util/simulator/weapons.ts
@@ -3,37 +3,43 @@ export default [
     name: 'steel',
     type: 'sword',
     damage: 80,
-    stars: 1
+    stars: 1,
+    price: 2000
   },
   {
     name: 'light',
     type: 'sword',
     damage: 82,
-    stars: 1
+    stars: 1,
+    price: 2020
   },
   {
     name: 'honor',
     type: 'sword',
     stars: 2,
-    damage: 89
+    damage: 89,
+    price: 2100
   },
   {
     name: 'time',
     type: 'sword',
     stars: 2,
-    damage: 89
+    damage: 89,
+    price: 2100
   },
   {
     name: 'shadows',
     type: 'sword',
     stars: 3,
-    damage: 97
+    damage: 97,
+    price: 2400
   },
   {
     name: 'destiny',
     type: 'sword',
     stars: 3,
-    damage: 98
+    damage: 98,
+    price: 2450
   },
   {
     name: 'death',
@@ -57,49 +63,57 @@ export default [
     name: 'truth',
     type: 'sword',
     damage: 81,
-    stars: 1
+    stars: 1,
+    price: 2010
   },
   {
     name: 'fire',
     type: 'broad_sword',
     damage: 86,
-    stars: 1
+    stars: 1,
+    price: 2150
   },
   {
     name: 'ice',
     type: 'broad_sword',
     damage: 86,
-    stars: 1
+    stars: 1,
+    price: 2150
   },
   {
     name: 'water',
     type: 'broad_sword',
     damage: 88,
-    stars: 1
+    stars: 1,
+    price: 2200
   },
   {
     name: 'glory',
     type: 'broad_sword',
     damage: 92,
-    stars: 2
+    stars: 2,
+    price: 2400
   },
   {
     name: 'courage',
     type: 'broad_sword',
     damage: 93,
-    stars: 2
+    stars: 2,
+    price: 2450
   },
   {
     name: 'justice',
     type: 'broad_sword',
-    damage: 99,
-    stars: 3
+    damage: 105,
+    stars: 3,
+    price: 2700
   },
   {
     name: 'bad_luck',
     type: 'broad_sword',
-    damage: 98,
-    stars: 3
+    damage: 104,
+    stars: 3,
+    price: 2690
   },
   {
     name: 'force',
