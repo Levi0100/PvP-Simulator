832eccc8e69fff1a4292cc4cb495e8fef661504a
Levi0100 <levi.pires2005@gmail.com>
2022-11-20 16:02:24 -0300

    removed `async` because is not necessary

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ const { resolve } = require('path')
 const { execSync } = require('child_process')
 const pathDist = resolve(__dirname, 'dist')
 
-const init = async () => {
+const init = () => {
   if (existsSync(pathDist)) rmSync(pathDist, { recursive: true })
   console.log('Building...')
   execSync('npm start')
